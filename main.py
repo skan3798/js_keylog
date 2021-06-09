544cd7119e03df3c761703b6fa1a593c22b6c8fa
Ubuntu <ubuntu@ip-172-31-44-167.ap-southeast-2.compute.internal>
2021-06-09 02:54:07 +0000

    update host and port

--- main.py
+++ main.py
@@ -54,5 +54,5 @@ def showKeylog():
   return res
 
 if __name__ == "__main__":
-  app.run(port=int("6060"), debug=True)
+  app.run(host="0.0.0.0", port=int("6060"), debug=True)
   #app.run(debug=True)
